f9781efd855debf8c32b653469a0764cb0493932
WhiteMirac1e <115178943+WhiteMirac1e@users.noreply.github.com>
2023-03-10 11:31:11 +0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,41 @@
+package JAVA_OOP_DZ2;
+
+public class Animal {
+
+    private double height;
+    private double weight;
+    private String eye_color;
+    private String voice;
+  
+    public Animal(double height, double weight, String eye_color, String voice) {
+        this.height = height;
+        this.weight = weight;
+        this.eye_color = eye_color;
+        this.voice = voice;
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public String getEyeColor() {
+        return this.eye_color;
+    }
+
+    public String getVoice() {
+        return this.voice;
+    }
+
+    public void roar() {
+        System.out.println("Голос: " + this.voice);
+    }
+
+    @Override
+    public String toString() {
+        String result = String.format("Рост: %d см\nВес: %d кг\nЦвет глаз: %s", this.height, this.weight, this.eye_color);
+        return result;
+    }
+}
