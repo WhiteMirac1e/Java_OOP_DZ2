31a9c702cc677e9087c94c9a4f52483419885f29
WhiteMirac1e <115178943+WhiteMirac1e@users.noreply.github.com>
2023-03-13 13:42:35 +0300

    Create Chicken.java

---
+++ Chicken.java
@@ -0,0 +1,12 @@
+public class Chicken extends Bird{
+
+    public Chicken(int height, int weight, String eye_color, String voice) {
+        super(height, weight, eye_color, voice, 2);
+    }
+
+    public Chicken() {
+        super(17, 2, "красный", "кудкудах", 2);
+}
+
+
+}
