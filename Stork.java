3b5c258147f23626b064c73568042b8ff8d95194
WhiteMirac1e <115178943+WhiteMirac1e@users.noreply.github.com>
2023-03-13 13:44:49 +0300

    Create Stork.java

---
+++ Stork.java
@@ -0,0 +1,11 @@
+public class Stork extends Bird {
+
+    public Stork(int height, int weight, String eye_color, String voice) {
+        super(height, weight, eye_color, voice, 2500);
+    }
+
+    public Stork() {
+        super(99, 4, "карий", "че-ли", 2500);
+    }
+
+}
